00472ce48750402dce704793c0e161c1b4494087
Piyan.Kotrey <korttry72@gmail.com>
2023-10-18 15:14:18 +0700

    add-config-server

---
+++ config_server/src/test/java/co/draxler/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.draxler.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
